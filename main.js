73a33c85a4f687c7e57bf614f04ba0ce72e186a1
jeanettelin <139308351+jeanettelin@users.noreply.github.com>
2023-07-29 18:06:38 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,27 +1,51 @@
-// JavaScript code for creating the bar chart (using D3.js version 7)
 d3.csv("https://raw.githubusercontent.com/jeanettelin/CS416DataViz_JeanetteLin.io/main/athleteEvents.csv").then(function(data) {
-  // Filter data to get only gold medals and group by country
-  var goldMedalsData = d3.rollup(
-    data,
-    function(d) { return d3.sum(d, function(d) { return d.Medal === "Gold" ? 1 : 0; }); },
-    function(d) { return d.NOC; }
-  );
+  // Function to update the chart based on the selected year
+  function updateChart(selectedYear) {
+    // Filter data to get only gold medals for the selected year and group by country
+    var goldMedalsData = d3.rollup(
+      data.filter(d => d.Year === selectedYear && d.Medal === "Gold"),
+      function(d) { return d.length; },
+      function(d) { return d.NOC; }
+    );
 
-  // Convert the rollup map to an array
-  goldMedalsData = Array.from(goldMedalsData, ([key, value]) => ({ key, value }));
+    // Convert the rollup map to an array
+    goldMedalsData = Array.from(goldMedalsData, ([key, value]) => ({ key, value }));
 
-  // Sort the data in descending order based on gold medals count
-  goldMedalsData.sort(function(a, b) {
-    return b.value - a.value;
-  });
+    // Sort the data in descending order based on gold medals count
+    goldMedalsData.sort(function(a, b) {
+      return b.value - a.value;
+    });
 
-  // Take only the top 10 countries
-  goldMedalsData = goldMedalsData.slice(0, 10);
+    // Take only the top 10 countries
+    goldMedalsData = goldMedalsData.slice(0, 10);
+
+    // Remove previous bars
+    chart.selectAll(".bar").remove();
+
+    // Create and append new bars to the chart
+    chart.selectAll(".bar")
+      .data(goldMedalsData)
+      .enter()
+      .append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d) { return x(d.key); })
+      .attr("y", function(d) { return y(d.value); })
+      .attr("width", x.bandwidth())
+      .attr("height", function(d) { return chartHeight - y(d.value); })
+      .attr("fill", "gold");
+
+    // Update x-axis and y-axis
+    chart.select(".x-axis").call(d3.axisBottom(x));
+    chart.select(".y-axis").call(d3.axisLeft(y));
+
+    // Update chart title
+    svg.select(".chart-title").text("Top 10 Countries with the Most Gold Medals in Summer Olympics - " + selectedYear);
+  }
 
   // Set up the SVG container
-  var svgWidth = 800; // Adjust the width here
-  var svgHeight = 500; // Adjust the height here
-  var margin = { top: 80, right: 20, bottom: 50, left: 60 }; // Increase the top margin
+  var svgWidth = 800;
+  var svgHeight = 500;
+  var margin = { top: 80, right: 20, bottom: 50, left: 60 };
   var chartWidth = svgWidth - margin.left - margin.right;
   var chartHeight = svgHeight - margin.top - margin.bottom;
 
@@ -36,39 +60,39 @@ d3.csv("https://raw.githubusercontent.com/jeanettelin/CS416DataViz_JeanetteLin.i
   // Set up scales
   var x = d3.scaleBand()
     .range([0, chartWidth])
-    .padding(0.2)
-    .domain(goldMedalsData.map(function(d) { return d.key; }));
+    .padding(0.2);
 
   var y = d3.scaleLinear()
-    .range([chartHeight, 0])
-    .domain([0, d3.max(goldMedalsData, function(d) { return d.value; })]);
-
-  // Create and append bars to the chart
-  chart.selectAll(".bar")
-    .data(goldMedalsData)
-    .enter()
-    .append("rect")
-    .attr("class", "bar")
-    .attr("x", function(d) { return x(d.key); })
-    .attr("y", function(d) { return y(d.value); })
-    .attr("width", x.bandwidth())
-    .attr("height", function(d) { return chartHeight - y(d.value); })
-    .attr("fill", "gold");
+    .range([chartHeight, 0]);
 
   // Append x-axis
   chart.append("g")
-    .attr("transform", "translate(0," + chartHeight + ")")
-    .call(d3.axisBottom(x));
+    .attr("class", "x-axis")
+    .attr("transform", "translate(0," + chartHeight + ")");
 
   // Append y-axis
   chart.append("g")
-    .call(d3.axisLeft(y));
+    .attr("class", "y-axis");
 
   // Add chart title
   svg.append("text")
+    .attr("class", "chart-title")
     .attr("x", svgWidth / 2)
     .attr("y", margin.top / 2)
     .attr("text-anchor", "middle")
     .style("font-size", "24px")
-    .text("Top 10 Countries with the Most Gold Medals in Summer Olympics");
+    .text("Top 10 Countries with the Most Gold Medals in Summer Olympics - 2008");
+
+  // Year slider event listener
+  var yearSlider = document.getElementById("yearSlider");
+  var selectedYearDisplay = document.getElementById("selectedYear");
+
+  yearSlider.addEventListener("input", function() {
+    var selectedYear = parseInt(yearSlider.value);
+    selectedYearDisplay.textContent = selectedYear;
+    updateChart(selectedYear);
+  });
+
+  // Initialize the chart with data for the year 2008
+  updateChart(2008);
 });
