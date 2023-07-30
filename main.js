267dd8b146fc2ae1fffd4a4fe3ef8c5e30fade69
jeanettelin <139308351+jeanettelin@users.noreply.github.com>
2023-07-29 19:41:08 -0700

    Update main.js

--- main.js
+++ main.js
@@ -42,71 +42,71 @@ d3.csv("https://raw.githubusercontent.com/jeanettelin/CS416DataViz_JeanetteLin.i
 
   // Function to update the chart based on the selected year
   function updateChart(selectedYear) {
-    // Filter data to get only medals for the selected year and group by country and gender
-    var medalsData = d3.rollup(
-      data.filter(d => d.year === selectedYear && d.medal !== ""), // Filter out rows without medals
-      function(d) { return d.length; },
-      function(d) { return d.noc; },
-      function(d) { return d.sex; }
-    );
+  // Filter data to get only medals for the selected year and group by country and gender
+  var medalsData = d3.rollup(
+    data.filter(d => d.Year === selectedYear && d.Medal !== ""), // Filter out rows without medals
+    function(d) { return d.length; },
+    function(d) { return d.NOC; },
+    function(d) { return d.Sex; }
+  );
 
-    // Convert the rollup map to an array and calculate the total medals count for each country
-    var medalsArray = Array.from(medalsData, ([country, genderData]) => ({
-      key: country,
-      totalMedals: d3.sum(genderData.values()),
-      femaleCount: genderData.get("F") || 0, // Get female medal count or set to 0 if not present
-    }));
+  // Convert the rollup map to an array and calculate the total medals count for each country
+  var medalsArray = Array.from(medalsData, ([country, genderData]) => ({
+    key: country,
+    totalMedals: d3.sum(genderData.values()),
+    femaleCount: genderData.get("F") || 0, // Get female medal count or set to 0 if not present
+  }));
 
-    // Sort the data in descending order based on total medals count
-    medalsArray.sort(function(a, b) {
-      return b.totalMedals - a.totalMedals;
+  // Sort the data in descending order based on total medals count
+  medalsArray.sort(function(a, b) {
+    return b.totalMedals - a.totalMedals;
+  });
+
+  // Take only the top 10 countries
+  medalsArray = medalsArray.slice(0, 10);
+
+  // Remove previous bars
+  chart.selectAll(".bar").remove();
+
+  // Update scales with new data
+  x.domain(medalsArray.map(function(d) { return d.key; }));
+  y.domain([0, d3.max(medalsArray, function(d) { return d.totalMedals; })]);
+
+  // Create and append new bars to the chart
+  var countryGroups = chart.selectAll(".country-group")
+    .data(medalsArray)
+    .enter()
+    .append("g")
+    .attr("class", "country-group")
+    .attr("transform", function(d) { return "translate(" + x(d.key) + ", 0)"; });
+
+  countryGroups.selectAll(".bar")
+    .data(d => [{ medal: "male", count: d.totalMedals - d.femaleCount }, { medal: "female", count: d.femaleCount }])
+    .enter()
+    .append("rect")
+    .attr("class", "bar")
+    .attr("x", 0)
+    .attr("y", d => y(d.count))
+    .attr("width", x.bandwidth())
+    .attr("height", d => chartHeight - y(d.count))
+    .attr("fill", d => (d.medal === "male" ? "#0084b4" : "#e68a00"))
+    .on("mouseover", function(d) {
+      tooltip.style("opacity", 1);
+      tooltip.html(`Medal: ${d.medal}<br>Count: ${d.count}<br>Total: ${d3.select(this.parentNode).datum().totalMedals}`)
+        .style("left", (d3.event.pageX + 10) + "px")
+        .style("top", (d3.event.pageY - 30) + "px");
+    })
+    .on("mouseout", function(d) {
+      tooltip.style("opacity", 0);
     });
 
-    // Take only the top 10 countries
-    medalsArray = medalsArray.slice(0, 10);
+  // Update x-axis and y-axis
+  chart.select(".x-axis").call(d3.axisBottom(x));
+  chart.select(".y-axis").call(d3.axisLeft(y));
 
-    // Remove previous bars
-    chart.selectAll(".bar").remove();
-
-    // Update scales with new data
-    x.domain(medalsArray.map(function(d) { return d.key; }));
-    y.domain([0, d3.max(medalsArray, function(d) { return d.totalMedals; })]);
-
-    // Create and append new bars to the chart
-    var countryGroups = chart.selectAll(".country-group")
-      .data(medalsArray)
-      .enter()
-      .append("g")
-      .attr("class", "country-group")
-      .attr("transform", function(d) { return "translate(" + x(d.key) + ", 0)"; });
-
-    countryGroups.selectAll(".bar")
-      .data(d => [{ medal: "male", count: d.totalMedals - d.femaleCount }, { medal: "female", count: d.femaleCount }])
-      .enter()
-      .append("rect")
-      .attr("class", "bar")
-      .attr("x", 0)
-      .attr("y", d => y(d.count))
-      .attr("width", x.bandwidth())
-      .attr("height", d => chartHeight - y(d.count))
-      .attr("fill", d => (d.medal === "male" ? "#0084b4" : "#e68a00"))
-      .on("mouseover", function(d) {
-        tooltip.style("opacity", 1);
-        tooltip.html(`Medal: ${d.medal}<br>Count: ${d.count}<br>Total: ${d3.select(this.parentNode).datum().totalMedals}`)
-          .style("left", (d3.event.pageX + 10) + "px")
-          .style("top", (d3.event.pageY - 30) + "px");
-      })
-      .on("mouseout", function(d) {
-        tooltip.style("opacity", 0);
-      });
-
-    // Update x-axis and y-axis
-    chart.select(".x-axis").call(d3.axisBottom(x));
-    chart.select(".y-axis").call(d3.axisLeft(y));
-
-    // Update chart title
-    svg.select(".chart-title").text(`Top 10 Countries with the Most Medals in Summer Olympics - ${selectedYear}`);
-  }
+  // Update chart title
+  svg.select(".chart-title").text(`Top 10 Countries with the Most Medals in Summer Olympics - ${selectedYear}`);
+}
 
   // Initialize the chart with data for the year 2008
   updateChart(2008);
